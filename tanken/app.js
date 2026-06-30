45375d1a3c1b5b503b48159a1eb3beb7e35f72eb
Oscar <oscar.rosner@web.de>
2026-06-30 15:12:47 +0200

    Add files via upload

--- tanken/app.js
+++ tanken/app.js
@@ -410,7 +410,7 @@ const stationTemplate = (s, czechPrice, fuelType) => {
       .map(
         (fuelType) => `
           <button
-            class="w-20 py-2 border rounded-lg transition-colors ${fuelType === activeFuelType ? "border-blue-600 border bg-blue-950 text-white font-semibold" : "border-transparent text-neutral-300 hover:text-neutral-100 "}"
+            class="w-24 py-2 border rounded-lg transition-colors ${fuelType === activeFuelType ? "border-blue-600 border bg-blue-950 text-white font-semibold" : "border-transparent text-neutral-300 hover:text-neutral-100 "}"
             data-fuel-type="${fuelType}"
             type="button"
           >
